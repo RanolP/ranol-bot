0e320fb853c3508442b1f8bff9471fcdd04dac0f
RanolP <public.ranolp@gmail.com>
2022-11-02 16:10:43 +0900

    a bit of lifetime issue

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use std::env;
+use std::{env, net::SocketAddr, sync::mpsc};
 
+use axum::{routing::get, Json, Router};
 use bot_any_telegram::{
     api::TelegramClient,
-    types::{ChatId, MessageContent, UpdateEvent},
+    types::{ChatId, MessageContent, Update, UpdateEvent},
 };
 use dotenvy::dotenv;
 use futures_lite::StreamExt;
 use kal::{
-    lex::{remove_leading, remove_trailing, CommandLexer, TokenTransformer, TransformHintProvider},
+    lex::{
+        remove_leading, remove_trailing, CommandLexer, TokenTransformError, TokenTransformer,
+        TransformHintProvider,
+    },
     Command, CommandParseError,
 };
 use ranol_bot::commands::RootCommand;
@@ -29,6 +33,63 @@ async fn main() {
         .map_err(|e| miette::miette!("{}: TELEGRAM_BOT_TOKEN", e))
         .unwrap_or_log();
     let telegram_client = TelegramClient::new(&token);
+
+    let handle_update = |event: UpdateEvent| {
+        async {
+            tracing::debug!("{:?}", &event);
+
+            match event {
+                UpdateEvent::Message { message } => match message.content {
+                    MessageContent::Text { text, entities: _ } => {
+                        let lexer = CommandLexer::new(&text);
+                        let fragments: Result<Vec<_>, _> = transformer.transform(lexer).collect();
+                        let command = fragments
+                            .as_ref()
+                            .map_err(CommandParseError::from)
+                            .and_then(|fragments| RootCommand::parse(fragments));
+
+                        match command {
+                            Ok(RootCommand::Ping(ping)) => {
+                                let message_write = ping.execute().await;
+                                reqores_client
+                                    .call(
+                                        telegram_client.send_message(
+                                            ChatId::Int(message.chat.id),
+                                            message_write,
+                                        ),
+                                    )
+                                    .await
+                                    .unwrap_or_log();
+                            }
+                            _ => {}
+                        }
+                    }
+                    content => {
+                        dbg!(content);
+                    }
+                },
+                e => {
+                    dbg!(e);
+                    // do not support now
+                }
+            }
+        }
+    };
+
+    let test = |Json(update): Json<Update>| async {
+        if let Some(event) = update.event {
+            todo!("lifetime issue");
+            // handle_update(event).await;
+        }
+    };
+
+    let router = Router::new().route("/telegram", get(test));
+    let socket_addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    axum::Server::bind(&socket_addr)
+        .serve(router.into_make_service())
+        .await
+        .unwrap_or_log();
+
     let mut long_polling = telegram_client.long_polling(|req| reqores_client.call(req));
 
     while let Some(event) = long_polling.next().await {
@@ -39,40 +100,6 @@ async fn main() {
                 continue;
             }
         };
-        tracing::debug!("{:?}", &event);
-
-        match event {
-            UpdateEvent::Message { message } => match message.content {
-                MessageContent::Text { text, entities: _ } => {
-                    let lexer = CommandLexer::new(&text);
-                    let fragments: Result<Vec<_>, _> = transformer.transform(lexer).collect();
-                    let command = fragments
-                        .as_ref()
-                        .map_err(CommandParseError::from)
-                        .and_then(|fragments| RootCommand::parse(fragments));
-
-                    match command {
-                        Ok(RootCommand::Ping(ping)) => {
-                            let message_write = ping.execute().await;
-                            reqores_client
-                                .call(
-                                    telegram_client
-                                        .send_message(ChatId::Int(message.chat.id), message_write),
-                                )
-                                .await
-                                .unwrap_or_log();
-                        }
-                        _ => {}
-                    }
-                }
-                content => {
-                    dbg!(content);
-                }
-            },
-            e => {
-                dbg!(e);
-                // do not support now
-            }
-        }
+        handle_update(event).await;
     }
 }
