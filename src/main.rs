28eeed37a3c6d97626c95844e7b3aa0b41e35121
RanolP <public.ranolp@gmail.com>
2022-11-06 00:38:15 +0900

    works on axum + dep version upgrade

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use kal::{
 };
 use ranol_bot::commands::RootCommand;
 use reqores_client_surf::SurfClient;
+use tokio::join;
 use tracing_unwrap::ResultExt;
 
 #[tokio::main]
@@ -26,13 +27,39 @@ async fn main() {
     );
 
     let reqores_client = SurfClient::new();
+    let reqores_client = &reqores_client;
     let token = env::var("TELEGRAM_BOT_TOKEN")
         .map_err(|e| miette::miette!("{}: TELEGRAM_BOT_TOKEN", e))
         .unwrap_or_log();
     let telegram_client = TelegramClient::new(&token);
+    let telegram_client = &telegram_client;
 
-    let handle_update = |event: UpdateEvent| {
-        async {
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel();
+
+    let tx_axum_path = tx.clone();
+    let test = move |Json(update): Json<Update>| async move {
+        if let Some(event) = update.event {
+            tx_axum_path.send(event).unwrap_or_log()
+        }
+    };
+
+    let router = Router::new().route("/telegram", get(test));
+    let socket_addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    let server_fut = axum::Server::bind(&socket_addr).serve(router.into_make_service());
+
+    let long_polling_fut = (move || async move {
+        let mut long_polling = telegram_client.long_polling(|req| reqores_client.call(req));
+
+        while let Some(event) = long_polling.next().await {
+            match event {
+                Ok(event) => tx.send(event).unwrap_or_log(),
+                Err(e) => tracing::error!("{:?}", e),
+            }
+        }
+    })();
+
+    let axum_req_fut = (move || async move {
+        while let Some(event) = rx.recv().await {
             tracing::debug!("{:?}", &event);
 
             match event {
@@ -71,32 +98,9 @@ async fn main() {
                 }
             }
         }
-    };
+    })();
 
-    let test = |Json(update): Json<Update>| async {
-        if let Some(event) = update.event {
-            todo!("lifetime issue");
-            // handle_update(event).await;
-        }
-    };
+    let (server, _, _) = join!(server_fut, long_polling_fut, axum_req_fut);
 
-    let router = Router::new().route("/telegram", get(test));
-    let socket_addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    axum::Server::bind(&socket_addr)
-        .serve(router.into_make_service())
-        .await
-        .unwrap_or_log();
-
-    let mut long_polling = telegram_client.long_polling(|req| reqores_client.call(req));
-
-    while let Some(event) = long_polling.next().await {
-        let event = match event {
-            Ok(event) => event,
-            Err(e) => {
-                tracing::error!("{:?}", e);
-                continue;
-            }
-        };
-        handle_update(event).await;
-    }
+    server.unwrap_or_log();
 }
